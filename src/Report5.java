d8d229c50eb60e4c751408ce5afb1cf905706953
Kuroshima Kei <e225718@ie.u-ryukyu.ac.jp>
2022-12-12 22:32:31 +0900

    3rd commit

--- src/Report5.java
+++ src/Report5.java
@@ -1,14 +1,10 @@
-import java.lang.NullPointerException;
-
 public class Report5{
     public static void main(String[] args){
-        try{
-            String name = null;
-            System.out.print(name.length());
-        } catch(NullPointerException e){
-            System.out.println("NullPointException が発生！！");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int n = Integer.parseInt(str);
+        System.out.println(n);
+
+
     }
 }
 
